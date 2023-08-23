7fcdd8daaf5d5694c2497a68b740e56959bf1075
Benedicta-107 <benedictaibeawuchi@gmail.com>
2023-08-23 21:07:30 +0000

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    result = 0
-    for x in set(my_list):
-        result += x
-    return (result)
+# 1-search_replace.py
+
+
+def search_replace(my_list, search, replace):
+    """Replace all occurrences of an element by another in a new list."""
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
